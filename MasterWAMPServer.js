dfcc72d3b00429de30ca276c6c9574c66d610658
Maciej Baj <macie.baj@gmail.com>
2017-04-19 12:32:59 +0200

    remove console logs

--- MasterWAMPServer.js
+++ MasterWAMPServer.js
@@ -19,7 +19,6 @@ class MasterWAMPServer extends WAMPServer {
 		this.socketCluster = socketCluster;
 
 		socketCluster.on('workerStart', worker => {
-			console.log('\x1b[33m%s\x1b[0m', 'MasterWAMPServer: on workerStart --- sending endpoints to new worker', this.endpoints);
 			this.reply(null, {
 				registeredEvents: Object.keys(this.endpoints.event),
 				type: schemas.MasterConfigRequestSchema.id,
@@ -28,14 +27,11 @@ class MasterWAMPServer extends WAMPServer {
 		});
 
 		socketCluster.on('workerMessage', (worker, request) => {
-			console.log('\x1b[33m%s\x1b[0m', 'MasterWAMPServer: on workerMessage ', request);
 			if (v.validate(request, schemas.MasterWAMPRequestSchema).valid &&
 				(request.type === schemas.MasterWAMPRequestSchema.id || request.type === schemas.InterProcessRPCRequestSchema.id)) {
-				console.log('\x1b[33m%s\x1b[0m', 'MasterWAMPServer: ON workerMessage ----- passed validation invoking RPC procedure:', request.type);
+
 				// request.type = schemas.MasterConfigRequestSchema.id;
 				this.processWAMPRequest(request, null);
-			} else {
-				console.log('\x1b[36m%s\x1b[0m', 'MasterWAMPServer ON workerMessage ----- WAMPRequestSchema.errors', v.validate(request, schemas.MasterWAMPRequestSchema).errors);
 			}
 		});
 	}
@@ -47,7 +43,7 @@ class MasterWAMPServer extends WAMPServer {
 	 * @param {*} data
 	 */
 	reply(socket, request, error, data) {
-		console.log('\x1b[36m%s\x1b[0m', 'MasterWAMPServer ----- reply payload', this.createResponsePayload(request, error, data));
+
 		return this.socketCluster.sendToWorker(request.workerId, this.createResponsePayload(request, error, data));
 	}
 
--- WAMPServer.js
+++ WAMPServer.js
@@ -20,11 +20,9 @@ class WAMPServer {
 	 * @returns {object} wampSocket
 	 */
 	upgradeToWAMP(socket) {
-		console.log('\x1b[36m%s\x1b[0m', 'WAMPServer ----- upgradeToWAMP', this.endpoints);
 
 		//register RPC endpoints
 		socket.on('raw', request => {
-			console.log('\x1b[36m%s\x1b[0m', 'WAMPServer ----- RECEIVED RPC CALL', request.procedure);
 			try {
 				request = JSON.parse(request);
 			} catch (ex) {
@@ -33,14 +31,13 @@ class WAMPServer {
 			if (v.validate(request, schemas.WAMPRequestSchema).valid && request.type === schemas.WAMPRequestSchema.id) {
 				this.processWAMPRequest(request, socket);
 			} else {
-				console.log('\x1b[36m%s\x1b[0m', 'WAMPServer processWAMPRequest WAMPRequestSchema.errors', v.validate(request, schemas.WAMPRequestSchema).errors);
+
 			}
 		});
 
 		//register Event endpoints
 		Object.keys(this.endpoints.event).forEach(event => {
 			socket.on(event, data => {
-				console.log('\x1b[36m%s\x1b[0m', 'WAMPServer ----- RECEIVED EVENT CALL', event, data);
 				this.processWAMPRequest({
 					type: schemas.EventRequestSchema.id,
 					procedure: event,
@@ -57,15 +54,13 @@ class WAMPServer {
 	 * @param {SocketCluster.Socket} socket
 	 */
 	processWAMPRequest(request, socket) {
-		console.log('\x1b[36m%s\x1b[0m', 'WampServer ----- processWAMPRequest received call procedure', request.procedure);
 		if (this.endpoints.rpc[request.procedure] && typeof this.endpoints.rpc[request.procedure] === 'function') {
-			console.log('\x1b[36m%s\x1b[0m', 'WampServer ----- processWAMPRequest RPC');
 			return this.endpoints.rpc[request.procedure](request.data, this.reply.bind(this, socket, request));
-		} else if (this.endpoints.event[request.procedure] && typeof this.endpoints.event[request.procedure] === 'function') {
-			console.log('\x1b[36m%s\x1b[0m', 'WampServer ----- processWAMPRequest Event');
+		}
+		else if (this.endpoints.event[request.procedure] && typeof this.endpoints.event[request.procedure] === 'function') {
 			return this.endpoints.event[request.procedure](request.data);
-		} else {
-			console.log('\x1b[36m%s\x1b[0m', 'WampServer ----- processWAMPRequest not registered. reply now');
+		}
+		else {
 			return this.reply(socket, request, `procedure ${request.procedure} not registered on WAMPServer`, null);
 		}
 	}
@@ -81,8 +76,6 @@ class WAMPServer {
 	}
 
 	createResponsePayload(request, error, data) {
-		console.log('\x1b[36m%s\x1b[0m', 'WampServer ----- createResponsePayload map for: ', request.type, schemas.reqToResMap[request.type]);
-
 		return Object.assign({}, request, {
 			success: !error,
 			data: data,
@@ -102,7 +95,6 @@ class WAMPServer {
 	 * @param {Map<RPCEndpoint>} endpoints
 	 */
 	registerRPCEndpoints(endpoints) {
-		console.log('\x1b[36m%s\x1b[0m', 'ENDPOINTS: registerRPCEndpoints', endpoints);
 		this.endpoints.rpc = Object.assign(this.endpoints.rpc, endpoints);
 	}
 
@@ -110,7 +102,6 @@ class WAMPServer {
 	 * @param {Map<RPCEndpoint>} endpoints
 	 */
 	registerEventEndpoints(endpoints) {
-		console.log('\x1b[36m%s\x1b[0m', 'ENDPOINTS: registerEVENTEndpoints', endpoints);
 		this.endpoints.event = Object.assign(this.endpoints.event, endpoints);
 	}
 
@@ -118,7 +109,6 @@ class WAMPServer {
 	 * @param {Map<RPCEndpoint>} endpoints
 	 */
 	reassignRPCEndpoints(endpoints) {
-		console.log('\x1b[36m%s\x1b[0m', 'ENDPOINTS: reassignPCEndpoints', endpoints);
 		this.endpoints.rpc = endpoints;
 	}
 
@@ -126,7 +116,6 @@ class WAMPServer {
 	 * @param {Map<RPCEndpoint>} endpoints
 	 */
 	reassignEventEndpoints(endpoints) {
-		console.log('\x1b[36m%s\x1b[0m', 'ENDPOINTS: reassignEventEndpoints', endpoints);
 		this.endpoints.event = endpoints;
 	}
 
