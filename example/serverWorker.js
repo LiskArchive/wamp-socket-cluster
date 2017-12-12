e8d3de7fdff5bf05a50e1dabb0aa9ce540d636cc
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2017-12-12 18:06:48 +0000

    Upgrade example to SC v9

--- example/serverWorker.js
+++ example/serverWorker.js
@@ -6,17 +6,22 @@ const eventEndpoints = {
 	multiplyByThree: num => console.info(`On server event action 'multiplyByThree': ${num} * 3 = ${num * 3}`),
 };
 
+const SCWorker = require('socketcluster/scworker');
 const WAMPServer = require('../WAMPServer');
 
-module.exports.run = (worker) => {
-	const scServer = worker.scServer;
+class Worker extends SCWorker {
+	run() {
+		const scServer = this.scServer;
 
-	const wampServer = new WAMPServer();
+		const wampServer = new WAMPServer();
 
-	scServer.on('connection', (socket) => {
-		wampServer.upgradeToWAMP(socket);
+		scServer.on('connection', (socket) => {
+			wampServer.upgradeToWAMP(socket);
 
-		wampServer.registerRPCEndpoints(rpcEndpoints);
-		wampServer.registerEventEndpoints(eventEndpoints);
-	});
-};
+			wampServer.registerRPCEndpoints(rpcEndpoints);
+			wampServer.registerEventEndpoints(eventEndpoints);
+		});
+	}
+}
+
+new Worker();
